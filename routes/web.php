d4e61704c6f27fcf9045b9c114ef9ebb45ad5490
farrelandika <farrelandika@gmail.com>
2026-03-05 12:34:21 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function () {
+    return 'Ini halaman profile';
+})->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/user', function () {
+        return 'Halaman User';
+    });
+
+    Route::get('/post', function () {
+        return 'Halaman Post';
+    });
+
+    Route::get('/event', function () {
+        return 'Halaman Event';
+    });
+
+});
+
+Route::prefix('admin')->group(function () {
+
+    Route::get('/user', function () {
+        return 'Admin User Page';
+    });
+
+    Route::get('/post', function () {
+        return 'Admin Post Page';
+    });
+
+    Route::get('/event', function () {
+        return 'Admin Event Page';
+    });
+
+});
+
+Route::redirect('/here', '/there');
+
+Route::view('/welcome', 'welcome');
