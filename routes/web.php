cb7898f0a216d37a1e8046a39e54e35f3c8a6e5b
farrelandika <farrelandika@gmail.com>
2026-03-08 20:51:47 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
 
 Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID '.$id;
 });
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
